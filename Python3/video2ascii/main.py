a106773c3509679f1e1647fea823e54ba5c071be
Ben Chislett <chislett.ben@gmail.com>
2019-06-04 00:24:57 -0230

    add working prototype of video to ascii converter

--- Python3/video2ascii/main.py
+++ Python3/video2ascii/main.py
@@ -1,27 +1,66 @@
 import numpy as np
 from PIL import Image, ImageSequence
+import cv2
+import time
 import curses
 
-from process_font import font_pixels, font_size, CHARS
+from process_font import font_image, font_size, CHARS
 
-VIDEO_FILE = 'test.wav'
+VIDEO_FILE = './test.mp4'
 
-stdscr = curses.initscr() # initialize the screen
-curses.noecho() # hide user key input
+stdscr = curses.initscr()  # initialize the screen
+curses.noecho()  # hide user key input
 
-vid = Image.open(VIDEO_FILE)
 
 def main():
-    for frame in ImageSequence.iterator(vid):
-        pass
+    vid = cv2.VideoCapture(VIDEO_FILE)
+    while (True):
+        ret, frame = vid.read()
+        if not ret:
+            break
+        frame_pixels = np.mean(np.asarray(frame), -1)
+        ratio = frame_pixels.shape[1] / \
+            frame_pixels.shape[0] * font_size[1] / font_size[0]
+        x = curses.COLS
+        y = curses.LINES
+        if (x > y):  # maintain aspect ratio
+            x = int(y * ratio)
+        else:
+            y = int(x * ratio)
+        block_x = frame_pixels.shape[1] // x
+        block_y = frame_pixels.shape[0] // y
+
+        font_arr = np.mean(np.asarray(font_image.resize(
+            (block_x * len(CHARS), block_y))), -1).astype(np.uint8)
+
+        means = []
+        for i in range(len(CHARS)):
+            means.append(np.mean(font_arr[:, i * block_x:(i+1) * block_x]))
+        means = list(map(lambda x: 255. * (x - min(means)) /
+                         (max(means) - min(means)), means))  # normalize to [0,255]
+
+        for i in range(x - 1):
+            for j in range(y - 1):
+                chunk = frame_pixels[j *
+                                     block_y:(j+1)*block_y, i*block_x:(i+1)*block_x]
+                #similarities = []
+                # for val in means:
+                #    similarities.append(abs(val - np.mean(chunk)))
+                #char = CHARS[similarities.index(min(similarities))]
+                char = CHARS[int(np.mean(chunk) / 255. * (len(CHARS) - 1))]
+                stdscr.addstr(j, i, char)
+        stdscr.refresh()
+
+
+# teardown
+def teardown():
+    curses.echo()
+    curses.endwin()
+
 
 # execute
 if __name__ == '__main__':
     try:
         main()
-    except:
-        pass
-
-# teardown
-curses.echo()
-curses.endwin()
+    finally:
+        teardown()
--- Python3/video2ascii/process_font.py
+++ Python3/video2ascii/process_font.py
@@ -1,24 +1,26 @@
 from PIL import Image, ImageFont, ImageDraw
 import numpy as np
 
-FONT_FILE = 'ubuntu_terminal.ttf'
+FONT_FILE = 'UbuntuMono-R.ttf'
 FONT_SIZE = 16
-BACKGROUND_COLOUR = (255,255,255)
-CHARS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%?&*()/\.";:,^'
+FONT_COLOUR = (255, 255, 255)
+BACKGROUND_COLOUR = (48, 10, 36)
+# CHARS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%?&*()/\.";:,^'
+CHARS = " .:-=+*#%@"
 
 font = ImageFont.truetype(font=FONT_FILE, size=FONT_SIZE)
-font_size = font.getsize()
-image_size = (font[0] * len(CHARS), font[1])
+font_size = font.getsize('A')  # monospace font
+image_size = (font_size[0] * len(CHARS), font_size[1])
 
-output = Image.new('RGB', image_size, BACKGROUND_COLOUR)
-draw = ImageDraw.Draw(output)
+font_image = Image.new('RGB', image_size, BACKGROUND_COLOUR)
+draw = ImageDraw.Draw(font_image)
 
-draw.text((0, 0), CHARS, font=font)
+draw.text((0, 0), CHARS, fill=FONT_COLOUR, font=font)
 
-font_pixels = np.asarray(output).astype(np.uint8)
 
 def main():
-    output.save(FONT_FILE.split('.')[0] + '.bmp', 'BMP')
+    font_image.save(FONT_FILE.split('.')[0] + '.bmp', 'BMP')
+
 
 if __name__ == '__main__':
     main()
