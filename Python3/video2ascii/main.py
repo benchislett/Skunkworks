1812e38751bcf116e7b128afb69ef98c255a21a3
Ben Chislett <chislett.ben@gmail.com>
2019-06-04 20:27:35 -0230

    settle on linear interpolation for video2ascii

--- Python3/video2ascii/main.py
+++ Python3/video2ascii/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+from skimage.measure import compare_ssim as ssim
 from PIL import Image, ImageSequence
 import cv2
 import time
@@ -31,23 +32,22 @@ def main():
         block_y = frame_pixels.shape[0] // y
 
         font_arr = np.mean(np.asarray(font_image.resize(
-            (block_x * len(CHARS), block_y))), -1).astype(np.uint8)
+            (block_x * len(CHARS), block_y))), -1)
 
-        means = []
+        char_zip = []
         for i in range(len(CHARS)):
-            means.append(np.mean(font_arr[:, i * block_x:(i+1) * block_x]))
-        means = list(map(lambda x: 255. * (x - min(means)) /
-                         (max(means) - min(means)), means))  # normalize to [0,255]
+            val = np.mean(font_arr[:, i * block_x:(i+1) * block_x])
+            char_zip.append((val, CHARS[i]))
+
+        char_zip.sort(key=lambda t: t[0])
+        chars_sorted = list(map(lambda t: t[1], char_zip))
 
         for i in range(x - 1):
             for j in range(y - 1):
                 chunk = frame_pixels[j *
                                      block_y:(j+1)*block_y, i*block_x:(i+1)*block_x]
-                #similarities = []
-                # for val in means:
-                #    similarities.append(abs(val - np.mean(chunk)))
-                #char = CHARS[similarities.index(min(similarities))]
-                char = CHARS[int(np.mean(chunk) / 255. * (len(CHARS) - 1))]
+                char = chars_sorted[int(
+                    np.mean(chunk) / 255. * (len(CHARS) - 1))]
                 stdscr.addstr(j, i, char)
         stdscr.refresh()
 
