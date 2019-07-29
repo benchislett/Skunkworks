cfcc7dac44080463d957e5d17be00cc14528e03e
Ben Chislett <chislett.ben@gmail.com>
2019-07-29 22:20:43 +0900

    [C][Langtons-Ant] Add game rules and drawing

--- C/Langtons-Ant/main.c
+++ C/Langtons-Ant/main.c
@@ -7,24 +7,34 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int width = 256;
-int height = 256;
+#define width 256
+#define height 256
 
 // Arguments with defaults
-unsigned int iterations = 1000;
+unsigned int max_iterations = 20000;
 
 // Set the global timestep counter
-long int timestep = 1;
+long timestep = 1;
 
 // Length of progress bar
-int barlen = 12;
+int barlen = 72;
+
+// Create the game state
+int field[width][height];
+short direction; // { 0: N, 1: E, 2: S, 3: W }
+long x;
+long y;
 
 // Surface for the main image, and the cairo instance
 cairo_surface_t *surface;
 cairo_t *cr;
 
-struct timespec start;
-struct timespec end;
+// Structure for colours
+typedef struct {
+  double r;
+  double g;
+  double b;
+} Colour;
 
 // Parse the command arguments into the global variables
 void parse_args(int argc, char *argv[])
@@ -35,12 +45,12 @@ void parse_args(int argc, char *argv[])
     switch (opt)
     {
     case 'i':
-      // Number of iterations to run the simulation
-      iterations = atoi(optarg);
+      // Number of iterations to run the simulation before exiting
+      max_iterations = atoi(optarg);
       break;
     default:
       // Print a help script if invalid arguments are entered
-      fprintf(stderr, "Usage: %s [-i iterations] \n", argv[0]);
+      fprintf(stderr, "Usage: %s [-i max_iters] \n", argv[0]);
       exit(EXIT_FAILURE);
     }
   }
@@ -52,36 +62,18 @@ void setup()
   // Create the context for the main image
   surface = cairo_image_surface_create(CAIRO_FORMAT_RGB24, width, height);
   cr = cairo_create(surface);
-}
 
-// Wipe the canvas
-void set_background_colour()
-{
-  cairo_set_source_rgb(cr, 0, 0, 0);
-  cairo_rectangle(cr, 0, 0, width, height);
-  cairo_fill(cr);
-}
-
-// Fix the background colour
-void draw()
-{
-  set_background_colour();
-}
-
-void show_progress(double i, double imax) {
-  printf("[");
-  int progress = barlen * (i / imax);
-  for (int j = 0; j < barlen; j++) {
-    printf(j <= progress ? "." : " ");
+  // Clear the field
+  for (int i = 0; i < width; i++) {
+    for (int j = 0; j < height; j++) {
+      field[i][j] = 0;
+    }
   }
-  printf("]\r");
-  fflush(stdout);
-}
 
-// Advance the simulation
-void step()
-{
-  draw();
+  // Set the starting position and orientation
+  x = width / 2;
+  y = height / 2;
+  direction = 0;
 }
 
 // Destroy, deallocate, and conclude operations
@@ -93,6 +85,117 @@ void teardown()
   cairo_surface_destroy(surface);
 
   printf("\n");
+  exit(0);
+}
+
+// Wipe the canvas
+void set_background_colour()
+{
+  cairo_set_source_rgb(cr, 0.0, 0.0, 0.0);
+  cairo_rectangle(cr, 0, 0, width, height);
+  cairo_fill(cr);
+}
+
+// Draw a single pixel
+void draw_pixel(long x, long y, Colour c) {
+  // Don't waste time drawing pixels that have the same colour as the background
+  if (c.r != 0.0 || c.g != 0.0 || c.b != 0.0) {
+    cairo_set_source_rgb(cr, c.r, c.g, c.b);
+    cairo_rectangle(cr, x, y, 1, 1);
+    cairo_fill(cr);
+  }
+}
+
+// Get the Colour of a given pixel
+Colour get_colour(int x, int y) {
+  switch (field[x][y]) {
+    case 0:
+      return (Colour){1.0, 1.0, 1.0};
+    case 1:
+      return (Colour){0.0, 0.0, 0.0};
+  }
+}
+
+// Fix the background colour
+void draw()
+{
+  set_background_colour();
+  for (int i = 0; i < width; i++) {
+    for (int j = 0; j < height; j++) {
+      draw_pixel(i, j, get_colour(i, j));
+    }
+  }
+}
+
+// Show a progress bar towards the iteration limit
+void show_progress(double i, double imax) {
+  printf("[");
+  int progress = barlen * (i / imax);
+  for (int j = 0; j < barlen; j++) {
+    printf(j <= progress ? "." : " ");
+  }
+  printf("]\r");
+  fflush(stdout);
+}
+
+// Rotate the ant n*90 degrees clockwise 
+void rotate_n(int n) {
+  while (n < 0) {
+    n += 4;
+  }
+
+  direction = (direction + n) % 4;
+}
+
+// Rotate the ant based on the colour of the tile
+void rotate() {
+  switch (field[x][y]) {
+    case 0:
+      rotate_n(1);
+      break;
+    case 1:
+      rotate_n(-1);
+      break;
+  }
+}
+
+// Modify the state of the tile the ant is currently standing on
+void flip() {
+  field[x][y] = (field[x][y] + 1) % 2;
+}
+
+// Advance the position of the ant a single unit in its current direction
+void walk() {
+  switch (direction) {
+    case 0:
+      y--;
+      break;
+    case 1:
+      x++;
+      break;
+    case 2:
+      y++;
+      break;
+    case 3:
+      x--;
+      break;
+  }
+}
+
+void check() {
+  if (x == 0 || x + 1 == width || y == 0 || y + 1 == height) {
+    teardown();
+  }
+}
+
+// Advance the simulation
+void step()
+{
+  rotate();
+  flip();
+  walk();
+  check();
+  draw();
 }
 
 int main(int argc, char *argv[])
@@ -104,16 +207,16 @@ int main(int argc, char *argv[])
   setup();
 
   // Main loop
-  for (int i = 0; i < iterations; i++)
+  for (int i = 0; i < max_iterations; i++)
   {
     // Advance evolution
     step();
 
     // Display progress bar
-    show_progress((double)i, (double)iterations);
+    show_progress((double)i, (double)max_iterations);
   }
 
-  // Finish up before exiting
+  // Finish up and exit
   teardown();
 
   return 0;
