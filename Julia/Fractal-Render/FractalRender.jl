e3bddbc473e5ff31ed3cc782c0e8c9d8f26e9365
benchislett <chislett.ben@gmail.com>
2019-08-21 23:56:22 +0900

    [Julia][Fractal-Render] Add main simulation loop
    References planned files for certain functionality

---
+++ Julia/Fractal-Render/FractalRender.jl
@@ -0,0 +1,30 @@
+module FractalRender
+
+  using CSFML.LibCSFML
+
+  include("./state.jl")
+  include("./colors.jl")
+  include("./simulate.jl")
+  include("./render.jl")
+
+  function main()
+    renderState = RenderState()
+    simState = State()
+
+    event = Ref(sfEvent(sfEvtClosed))
+    while Bool(sfRenderWindow_isOpen(renderState.window))
+      while Bool(sfRenderWindow_pollEvent(renderState.window, event))
+        if event.x.type == sfEvtClosed
+          sfRenderWindow_close(renderState.window)
+        end
+      end
+
+      step!(simState)
+
+      render!(simState, renderState)
+    end
+  end
+end
+
+FractalRender.main()
+
