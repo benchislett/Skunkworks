da9ebcf9427c33d16010f6c643207dfedfae05d1
benchislett <chislett.ben@gmail.com>
2019-09-07 15:48:56 -0400

    [Python3][ResNet-18] Use STL10 and Adam optimizer

--- Python3/ResNet-18/train.py
+++ Python3/ResNet-18/train.py
@@ -10,18 +10,18 @@ toTensor = torchvision.transforms.Compose([
     torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
 
-dataset_train = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=toTensor)
+dataset_train = torchvision.datasets.STL10(root='./data', split="train", download=True, transform=toTensor)
 train_loader = torch.utils.data.DataLoader(dataset_train, batch_size=128, shuffle=True)
 
-dataset_test = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=toTensor)
+dataset_test = torchvision.datasets.STL10(root='./data', split="test", download=True, transform=toTensor)
 test_loader = torch.utils.data.DataLoader(dataset_test, shuffle=True)
 
 model = ResNet18(10).cuda()
 
 loss = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-for epoch in range(4):
+for epoch in range(64):
     loss_acc = 0.0
     num_batches = 0
     for i, batch in enumerate(train_loader):
@@ -37,8 +37,6 @@ for epoch in range(4):
         loss_tmp = loss_batch.item()
         loss_acc += loss_tmp
         num_batches += 1
-
-        print("Epoch: {}\tBatch: {}, Batch Loss: {}".format(epoch, i, loss_tmp))
 
     print("\n\nEpoch {} Complete! Loss: {}\n\n".format(epoch, loss_acc / num_batches))
 
