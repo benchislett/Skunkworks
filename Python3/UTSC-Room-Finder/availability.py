a8b6d04802a3cb4d5e15fb12d5497b96fc879f46
benchislett <chislett.ben@gmail.com>
2019-11-22 15:59:41 -0500

    Add check for calendar overflow and sort rooms

--- Python3/UTSC-Room-Finder/availability.py
+++ Python3/UTSC-Room-Finder/availability.py
@@ -1,3 +1,6 @@
+from typing import Dict, List, Tuple
+
+
 def get_available_rooms(availability_map: Dict[str, Dict[int, List[str]]], day: int, timeslot: int) -> List[Tuple[str, str]]:
     """Return a list of available rooms and closing times for a given weekday day,
     timeslot (in UTSC Calendar format) and availability_map
@@ -7,11 +10,12 @@ def get_available_rooms(availability_map: Dict[str, Dict[int, List[str]]], day: 
         try:
             if availability_map[room][day][timeslot] == 'Empty':
                 length = 1
-                while availability_map[room][day][timeslot + length] == 'Empty':
+                while timeslot + length < 28 and availability_map[room][day][timeslot + length] == 'Empty':
                     length += 1
                 close_time = str((timeslot + length) // 2 + 8) + \
                     str(':00' if (timeslot + length) % 2 == 0 else ':30')
                 rooms.append((room, close_time))
         except:
             pass
+    rooms.sort()
     return rooms
