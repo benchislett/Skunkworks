12775ed525c8bac84c06dc4ccdbc7d23365eadf9
benchislett <chislett.ben@gmail.com>
2019-11-22 15:23:48 -0500

    Add complete scraping in parse.py

--- Python3/UTSC-Room-Finder/parse.py
+++ Python3/UTSC-Room-Finder/parse.py
@@ -2,19 +2,40 @@ from typing import List, Dict
 from bs4 import BeautifulSoup
 import datetime
 
-def get_availability_map(date_str: str, room_to_html: Dict[str, str]) -> Dict[str, Dict[str, str]]:
+def get_availability_map(room_to_html: Dict[str, str]) -> Dict[str, Dict[str, str]]:
     """Return a dictionary containing mappings from room names
-    to a dictionary of available start times to the end of that availability
-    period, on day date_str (ISO format).
+    to a dictionary of days to list of strings representing availability at given times,
+    from 8:00 to 21:00 inclusive, from the html table room_to_html.
+    Availability will either be "Empty", or the listed event.
     """
+    out_map = {}
+    for room in {'BV-363': 12}:#room_to_html:
+        out_map[room] = {}
 
-    for room in room_to_html:
         html = room_to_html[room]
         soup = BeautifulSoup(html, 'html.parser')
-        day = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-        for row in soup.findAll('tr'):
-            contents = row.findAll('td')
-            if len(contents) > 3:
-                print(contents[1].contents)
 
+        days = "Mon Tue Wed Thu Fri Sat Sun".split(' ')
+        for day in days:
+            out_map[room][day] = []
+            timeout = 0
+            current_event = None
+            for row in soup.findAll('tr'):
+                contents = row.findAll('td')
+                if contents is None or len(contents) < 2:
+                    continue
+                if timeout > 0:
+                    timeout -= 1
+                    out_map[room][day].append(current_event)
+                else:
+                    item = contents[1].extract()
+                    print(item)
+                    if item.has_attr('rowspan') and item['rowspan'] is not None:
+                        print(item['rowspan'])
+                        timeout = int(item['rowspan']) - 1
+                        current_event = item.get_text(separator=" ")
+                        out_map[room][day].append(current_event)
+                    else:
+                        out_map[room][day].append('Empty')
 
+    return out_map
