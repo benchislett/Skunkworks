29fda49d392071386e6afc979adf96745eb75862
benchislett <chislett.ben@gmail.com>
2019-12-03 01:20:41 -0500

    [Python3][Conv-Autoencoder] Remove parallel loader
    Since we're loading data onto the gpu, parallelism isn't allowed, and so
    the workers argument has to be removed.

--- Python3/Conv-Autoencoder/data.py
+++ Python3/Conv-Autoencoder/data.py
@@ -3,7 +3,7 @@ import torchvision
 import torchvision.datasets as datasets
 
 
-def get_celeba_loaders(root='~/.ml_data', batch_size=64, num_workers=2):
+def get_celeba_loaders(root='~/.ml_data', batch_size=64):
     """Return a tuple containing the attribute names as a tuple,
     and the train, test, and validation data loaders for the
     CelebA dataset from torchvision.
@@ -19,14 +19,11 @@ def get_celeba_loaders(root='~/.ml_data', batch_size=64, num_workers=2):
 
     train_loader = torch.utils.data.DataLoader(train_dset,
                                                batch_size=batch_size,
-                                               shuffle=True,
-                                               num_workers=num_workers)
+                                               shuffle=True)
     test_loader = torch.utils.data.DataLoader(test_dset,
                                               batch_size=batch_size,
-                                              shuffle=False,
-                                              num_workers=num_workers)
+                                              shuffle=False)
     val_loader = torch.utils.data.DataLoader(val_dset,
                                              batch_size=batch_size,
-                                             shuffle=False,
-                                             num_workers=num_workers)
+                                             shuffle=False)
     return (tuple(train_dset.attr_names), train_loader, test_loader, val_loader)
