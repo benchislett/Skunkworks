dae7eae382f1f3a650a00024554bf68da26afbf0
benchislett <chislett.ben@gmail.com>
2019-12-03 01:22:06 -0500

    [Python3][Conv-Autoencoder] Fix model
    Add padding so that shape isn't lost where we don't want it to be.
    Store some image data on the model class for handy reference
    View the data in and out of encoding to work with linear layers.

--- Python3/Conv-Autoencoder/model.py
+++ Python3/Conv-Autoencoder/model.py
@@ -6,23 +6,28 @@ class CAE(nn.Module):
     def __init__(self, code_bits=1024, width=32, height=32):
         super(CAE, self).__init__()
 
+        self.w = width
+        self.h = height
+        self.imgsize = width * height * 3
+        self.code_bits = code_bits
+
         # Encoder
-        self.conv1 = nn.Conv2d(3, 24, 3, stride=1)
-        self.conv2 = nn.Conv2d(24, 32, 3, stride=1)
-        self.conv3 = nn.Conv2d(32, 32, 3, stride=1)
-        self.conv4 = nn.Conv2d(32, 32, 3, stride=1)
-        self.conv5 = nn.Conv2d(32, 48, 3, stride=1)
-        self.conv6 = nn.Conv2d(48, 3, 3, stride=1)
-        self.linear_in = nn.Linear(width * height * 3, code_bits)
+        self.conv1 = nn.Conv2d(3, 24, 3, padding=1, stride=1)
+        self.conv2 = nn.Conv2d(24, 32, 3, padding=1, stride=1)
+        self.conv3 = nn.Conv2d(32, 32, 3, padding=1, stride=1)
+        self.conv4 = nn.Conv2d(32, 32, 3, padding=1, stride=1)
+        self.conv5 = nn.Conv2d(32, 48, 3, padding=1, stride=1)
+        self.conv6 = nn.Conv2d(48, 3, 3, padding=1, stride=1)
+        self.linear_in = nn.Linear(self.imgsize, code_bits)
 
         # Decoder
-        self.linear_out = nn.Linear(code_bits, width * height * 3)
-        self.deconv1 = nn.ConvTranspose2d(3, 48, 3, stride=1)
-        self.deconv2 = nn.ConvTranspose2d(48, 32, 3, stride=1)
-        self.deconv3 = nn.ConvTranspose2d(32, 32, 3, stride=1)
-        self.deconv4 = nn.ConvTranspose2d(32, 32, 3, stride=1)
-        self.deconv5 = nn.ConvTranspose2d(32, 24, 3, stride=1)
-        self.deconv6 = nn.ConvTranspose2d(24, 3, 3, stride=1)
+        self.linear_out = nn.Linear(code_bits, self.imgsize)
+        self.deconv1 = nn.ConvTranspose2d(3, 48, 3, padding=1, stride=1)
+        self.deconv2 = nn.ConvTranspose2d(48, 32, 3, padding=1, stride=1)
+        self.deconv3 = nn.ConvTranspose2d(32, 32, 3, padding=1, stride=1)
+        self.deconv4 = nn.ConvTranspose2d(32, 32, 3, padding=1, stride=1)
+        self.deconv5 = nn.ConvTranspose2d(32, 24, 3, padding=1, stride=1)
+        self.deconv6 = nn.ConvTranspose2d(24, 3, 3, padding=1, stride=1)
 
     def encode(self, x):
         x = F.relu(self.conv1(x))
@@ -31,11 +36,11 @@ class CAE(nn.Module):
         x = F.relu(self.conv4(x))
         x = F.relu(self.conv5(x))
         x = F.relu(self.conv6(x))
-        x = F.sigmoid(self.linear_in(x))
+        x = F.sigmoid(self.linear_in(x.view(-1, self.imgsize)))
         return x
 
     def decode(self, x):
-        x = F.relu(self.linear_out(x))
+        x = F.relu(self.linear_out(x)).view(-1, 3, self.w, self.h)
         x = F.relu(self.deconv1(x))
         x = F.relu(self.deconv2(x))
         x = F.relu(self.deconv3(x))
