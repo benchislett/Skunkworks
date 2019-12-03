fd9f8d87b3f215c7021b9c3ebad67c229e38c146
benchislett <chislett.ben@gmail.com>
2019-12-03 01:23:34 -0500

    [Python3][Conv-Autoencoder] Add main executor
    Currently just trains.
    TODO:
    - fix memory issues (preferably by shrinking the model to preserve batch
    size)
    - add test and valid executors in main.py
    - refactor logging for more smooth output between batches/epochs
    - add output visualization (save output images, display interactively
    with matplotlib, etc.)

---
+++ Python3/Conv-Autoencoder/main.py
@@ -0,0 +1,27 @@
+import torch
+import torchvision
+import os
+
+torch.cuda.set_device(0)
+torch.set_default_tensor_type('torch.cuda.FloatTensor')
+
+from data import get_celeba_loaders
+from model import CAE
+from test import test
+from train import train
+
+batch_size = 8
+num_epochs = 8
+
+attr_names, train_loader, test_loader, val_loader = get_celeba_loaders(
+    batch_size=batch_size)
+
+model = CAE(code_bits=1024, width=218, height=178)
+
+loss = torch.nn.MSELoss()
+optimizer = torch.optim.SGD(
+    model.parameters(), lr=0.01, momentum=0.99, weight_decay=0.0001)
+
+for epoch in range(num_epochs):
+    epoch_loss = train(model, loss, optimizer, train_loader)
+    print("[Epoch {}] Loss: {}".format(epoch, epoch_loss))
