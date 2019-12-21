a8fac09a56b1413f75e68069229f2e29520b506e
benchislett <chislett.ben@gmail.com>
2019-12-21 15:27:01 -0330

    [Python3][Noise] Generalize Perlin Noise

--- Python3/Noise/perlin.py
+++ Python3/Noise/perlin.py
@@ -1,4 +1,6 @@
 import numpy as np
+import itertools
+from operator import sub
 
 def fade(t):
     return 6 * t ** 5 - 15 * t ** 4 + 10 * t ** 3
@@ -16,43 +18,64 @@ def scale(x, new_min, new_max):
     return x
 
 
+def scale_ndarray(x, new_shape):
+    factors = tuple(i // j for (i, j) in zip(new_shape, x.shape))
+    y = np.zeros(new_shape)
+    
+    for slice_starts in itertools.product(*(range(i) for i in factors)):
+        slices = tuple(slice(i, j, k) for (i, j, k) in zip(slice_starts, new_shape, factors))
+        y[slices] = x
+
+    return y
+
+
+def merge_vecs(vecs, weights):
+    acc = vecs.copy()
+    r = 0
+    while len(acc) > 1:
+        for i in range(len(acc) // 2):
+            w = weights[r]
+            acc[i] = lerp(acc[i], acc[len(acc) // 2 + i], w)
+        acc = acc[:len(acc) // 2]
+        r += 1
+    return acc[0]
+
+
 def perlin_noise(res, boxes):
-    w, h = res
-    num_boxes_x, num_boxes_y = boxes
-    box_x, box_y = w // num_boxes_x, h // num_boxes_y
+    n = len(res)
+    dims = np.asarray(res)
+    boxes = np.asarray(boxes)
+    box_sizes = dims // boxes
 
-    angles = 2 * np.pi * np.random.rand(num_boxes_y + 1, num_boxes_x + 1)
-    gradients = np.dstack((np.cos(angles), np.sin(angles)))
+    grad_dims = tuple(boxes + 1) + (n,)
 
-    noise = np.zeros((h, w))
+    gradients = np.random.normal(size=grad_dims)
+    gradients /= np.linalg.norm(gradients, axis=-1, keepdims=True)
 
-    for y in range(h):
-        for x in range(w):
-            i = x // box_x
-            j = y // box_y
+    noise = np.zeros(res)
 
-            u = (x / box_x) % 1
-            v = (y / box_y) % 1
+    indexes = tuple(slice(0, i) for i in dims)
+    grid = np.moveaxis(np.mgrid[indexes], 0, -1)
+    grid_boxes = grid // box_sizes
+    grid_fracs = (grid / box_sizes) % 1
+    
+    weights = np.moveaxis(fade(grid_fracs), -1, 0)
 
-            weight_x, weight_y = fade(u), fade(v)
+    dotted_vecs = []
+    for diff in itertools.product((0, 1), repeat=n):
+        indexes = tuple(slice(i, j) for (i, j) in zip(diff, tuple(map(lambda x, y: x - (1 - y), gradients.shape, diff))))
+        grads = scale_ndarray(gradients[indexes], grid.shape)
+        dists = grid_fracs - diff
+        dotted_vecs.append(np.einsum('ijk,ijk->ij', grads, dists))
 
-            dotted_vecs = []
-            for dx, dy in [(0, 0), (0, 1), (1, 0), (1, 1)]:
-                grad = gradients[j + dy, i + dx, :]
-                dist = [u - dx, v - dy]
-                dotted_vecs.append(grad.dot(dist))
-
-            lerp_top = lerp(dotted_vecs[0], dotted_vecs[2], weight_x)
-            lerp_bot = lerp(dotted_vecs[1], dotted_vecs[3], weight_x)
-
-            noise[y, x] = lerp(lerp_top, lerp_bot, weight_y)
+    noise = merge_vecs(dotted_vecs, weights)
 
     return scale(noise, -1, 1)
 
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
-    
+
     res = [256, 256]
     boxes = [4, 4]
     noise = perlin_noise(res, boxes)
