61ccd115018dfb23de7044a405f8fa6c9fdabd65
benchislett <chislett.ben@gmail.com>
2020-08-07 08:07:15 -0230

    Add AppBar and HomePage with clock and greeting

--- TypeScript/chrome-dashboard/src/App.tsx
+++ TypeScript/chrome-dashboard/src/App.tsx
@@ -2,12 +2,63 @@ import React from 'react';
 import './App.css';
 
 import { Background } from './Background/Background';
+import { AppBar } from './AppBar/AppBar';
+import { HomePage } from './HomePage/HomePage';
+
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#ffffff'
+    },
+    secondary: {
+      main: '#000000'
+    }
+  },
+  props: {
+    MuiSvgIcon: {
+      htmlColor: '#ffffff'
+    },
+    MuiTypography: {
+      color: 'primary'
+    }
+  }
+});
+
+enum Pages {
+  HOME
+}
+
+interface PageProps {
+  children: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function Page({ children, index, value }: PageProps) {
+  const style = Object.assign(index === value ? {} : { display: 'none' }, {
+    width: '100%',
+    height: '100%'
+  });
+  return <div style={style}>{children}</div>;
+}
 
 function App() {
+  const [page, setPage] = React.useState(Pages.HOME);
+
   return (
-    <div className="App">
-        <Background />
-    </div>
+    <>
+      <Background />
+      <ThemeProvider theme={theme}>
+        <div className='App'>
+          <AppBar changePage={setPage} />
+          <Page index={0} value={page}>
+            <HomePage />
+          </Page>
+        </div>
+      </ThemeProvider>
+    </>
   );
 }
 
--- TypeScript/chrome-dashboard/src/Background/Background.tsx
+++ TypeScript/chrome-dashboard/src/Background/Background.tsx
@@ -6,24 +6,32 @@ function Background() {
   const [image, setImage] = React.useState(null);
 
   React.useEffect(() => {
-    fetch('https://source.unsplash.com/1920x1080/?Wallpaper').then(response => {
-      if (!response.ok) {
-        throw Error("Error fetching image!");
-      }
-      return response.url;
-    }).then(data => setImage(data as any)).catch(err => {
-      throw Error(err.message);
-    })
+    fetch('https://source.unsplash.com/1920x1080/?Wallpaper')
+      .then((response) => {
+        if (!response.ok) {
+          throw Error('Error fetching image!');
+        }
+        return response.url;
+      })
+      .then((data) => setImage(data as any))
+      .catch((err) => {
+        throw Error(err.message);
+      });
   }, []);
 
-  if (image == null) {return <div></div>;}
-
-  else {
+  if (image == null) {
+    return <div></div>;
+  } else {
     console.log(image);
-    return (<div style={{height: '100%'}}>
-      <div className='background-image' style={{backgroundImage: `url(${image})`}} />
-  </div>);
-    }
+    return (
+      <div style={{ height: '100%' }}>
+        <div
+          className='background-image'
+          style={{ backgroundImage: `url(${image})` }}
+        />
+      </div>
+    );
+  }
 }
 
 export { Background };
